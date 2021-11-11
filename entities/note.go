47e0293734af539603647e60c36a7817d13b5155
bjjackson32@gmail.com <liquidB83962!j>
2021-11-11 13:10:52 -0500

    Note functionality and tests

--- entities/note.go
+++ entities/note.go
@@ -1,7 +1,64 @@
 package entities
 
+import (
+	"encoding/json"
+	"errors"
+	"strings"
+	"time"
+)
+
+const TimeFormat = time.RFC822
+
 type Note struct {
 	CharacterId string `json:"character_id"`
 	Body        string `json:"body"`
 	Date        string `json:"date"`
+}
+
+// Marshal converts the note into JSON bytes
+func (n Note) Marshal() ([]byte, error) {
+	err := n.Valid()
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(n)
+}
+
+// Unmarshal parses json data into a note
+func (n *Note) Unmarshal(jsonBytes []byte) error {
+	err := json.Unmarshal(jsonBytes, &n)
+	if err != nil {
+		return err
+	}
+	return n.Valid()
+}
+
+// Valid checks if the note has the appropriate fields.
+// Returns nil if valid
+func (n Note) Valid() error {
+	var b strings.Builder
+	if n.CharacterId == "" {
+		b.WriteString("no character id for note\n")
+	}
+	_, err := time.Parse(TimeFormat, n.Date)
+	if err != nil {
+		b.WriteString("bad time format\n")
+	}
+	if n.Body == "" {
+		b.WriteString("no body\n")
+	}
+	if b.Len() != 0 {
+		return errors.New(b.String())
+	}
+	return nil
+}
+
+// Touch updates the note to the current time
+func (n *Note) Touch() {
+	n.Date = time.Now().Format(TimeFormat)
+}
+
+// String converts the note to a formatted string
+func (n Note) String() string {
+	return n.Date + "\n" + n.Body + "\n"
 }
---
+++ entities/note_test.go
@@ -0,0 +1,42 @@
+package entities
+
+import "testing"
+
+func TestNote_Serialization(t *testing.T) {
+	note := Note{CharacterId: "bruce", Body: "Test Note"}
+	note.Touch()
+	t.Log(note)
+
+	jsonBytes, err := note.Marshal()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	var note2 Note
+
+	err = note2.Unmarshal(jsonBytes)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	var note3 Note
+
+	err = note3.Unmarshal([]byte{})
+
+	if err == nil {
+		t.Error("failed to catch json error")
+	}
+
+}
+
+func TestNote_Valid(t *testing.T) {
+	note := Note{}
+
+	_, err := note.Marshal()
+	t.Log(err)
+	if err == nil {
+		t.Error("failed to catch errors")
+	}
+}
