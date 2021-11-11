e61ea7a752a2a077501c8625a1eb089c14085b1c
bjjackson32@gmail.com <liquidB83962!j>
2021-11-11 13:37:15 -0500

    Weapon Entity, functionality, and tests

---
+++ entities/weapon.go
@@ -0,0 +1,54 @@
+package entities
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+type Weapon struct {
+	Name        string           `json:"name"`
+	Description string           `json:"description"`
+	Properties  []WeaponProperty `json:"properties,string,omitempty"`
+}
+
+type WeaponProperty string
+
+const (
+	Finesse   WeaponProperty = "Finesse"
+	Heavy     WeaponProperty = "Heavy"
+	Light     WeaponProperty = "Light"
+	Loading   WeaponProperty = "Loading"
+	Range     WeaponProperty = "Range"
+	Reach     WeaponProperty = "Reach"
+	Special   WeaponProperty = "Special"
+	Thrown    WeaponProperty = "Thrown"
+	TwoHanded WeaponProperty = "Two Handed"
+	Versatile WeaponProperty = "Versatile"
+)
+
+func (w Weapon) Marshal() ([]byte, error) {
+	return json.Marshal(w)
+}
+
+func (w *Weapon) Unmarshal(jsonBytes []byte) error {
+	return json.Unmarshal(jsonBytes, &w)
+}
+
+func (w Weapon) PropertiesString() string {
+	var props []string
+	for _, prop := range w.Properties {
+		props = append(props, string(prop))
+	}
+	if len(props) == 0 {
+		return ""
+	}
+	return strings.Join(props, " | ")
+}
+
+func (w Weapon) String() string {
+	var b strings.Builder
+	b.WriteString(w.Name + "\n")
+	b.WriteString(w.PropertiesString() + "\n")
+	b.WriteString(w.Description + "\n")
+	return b.String()
+}
---
+++ entities/weapon_test.go
@@ -0,0 +1,39 @@
+package entities
+
+import "testing"
+
+func TestWeapon_Serialization(t *testing.T) {
+	weapon := Weapon{
+		Name:        "Sword",
+		Description: "1d6 damage",
+		Properties:  []WeaponProperty{Finesse, Light},
+	}
+	jsonBytes, err := weapon.Marshal()
+
+	if err != nil {
+		t.Error(err)
+	}
+	var weapon2 Weapon
+
+	err = weapon2.Unmarshal(jsonBytes)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(weapon2)
+
+	var weapon3 Weapon
+
+	err = weapon3.Unmarshal([]byte{})
+
+	if err == nil {
+		t.Error("failed to catch json error")
+	}
+
+	weapon4 := Weapon{
+		Name:        "Sword",
+		Description: "1d6 damage",
+	}
+
+	weapon4.PropertiesString()
+}
